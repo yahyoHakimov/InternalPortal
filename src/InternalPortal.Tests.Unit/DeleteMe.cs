f9d7a6f4fd934f51bcbf309dbc2a115b642c767a
hakim <yahyohakimov7@gmail.com>
2024-09-09 06:09:49 +0500

    INFRA: Initialize unit test project.

---
+++ src/InternalPortal.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternalPortal.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(condition: true);
+    }
+}
